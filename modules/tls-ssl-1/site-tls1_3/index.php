040245b1b5a770cef10cf9ed186287323d676293
richard <richard.de.munck@accenture.com>
2024-03-04 13:36:49 +0100

    added 2 xss challenges

--- modules/tls-ssl-1/site-tls1_3/index.php
+++ modules/tls-ssl-1/site-tls1_3/index.php
@@ -10,31 +10,64 @@
 <h1>Welcome to TLS 1.3</h1>
 <h2>Casual reflected xss</h2>
 
-<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="post">
   Name: <input type="text" name="name">
+  <input type="hidden" name="action" value="casual_xss">
   <input type="submit">
 </form>
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_POST['name'];
-    echo "Hello, " . $name;
-}
-?>
+<h2>lenght limited reflected xss</h2>
+
+<form method="post">
+  Name (max 20 characters): <input type="text" name="name" maxlenght="15">
+  <input type="hidden" name="action" value="length_xss">
+  <input type="submit">
+</form>
+
 
 <h2>Hidden reflected xss</h2>
 
-<form hidden method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form hidden method="post">
   Name: <input type="text" name="name">
+  <input type="hidden" name="action" value="hidden_xss">
   <input type="submit">
 </form>
 
+
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // collect value of input field
-    $name = $_POST['name'];
+    $action = $_POST['action'];
+
+    switch ($action) {
+        case 'length_xss':
+            length_xss();
+            break;
+        case 'casual_xss':
+            casual_xss();
+            break;
+        case 'hidden_xss':
+            casual_xss();
+            break;
+        default:
+            echo "something went wrong";
+            break;
+    }
+}
+
+function length_xss(){
+  // example payload: <svg/onload=alert()>
+  $name = $_POST['name'];
+  if(strlen($name) <= 20){
     echo "Hello, " . $name;
+  }
+  else{
+    echo "name is too long";
+  }
+}
+
+function casual_xss(){
+$name = $_POST['name'];
+echo "Hello, " . $name;
 }
 ?>
 
