d5e6f9c8ded8125349728c3a95ad48b36a1a62c0
richard <richard.de.munck@accenture.com>
2024-03-05 16:14:23 +0100

    added dos challange that delays per character

--- modules/denial-of-service/site-denial-of-service/index.php
+++ modules/denial-of-service/site-denial-of-service/index.php
@@ -3,9 +3,10 @@
 <html>
 
 <head>
-        <title>TLS 1.3</title>
+        <title>Denial of Service</title>
 </head>
-
+<body>
+<h2>Denial of service - encoding</h2>
 <form method="post">
     <div class="container">
       <label for="uname"><b>Username</b></label>
@@ -13,19 +14,60 @@
   
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
-  
+      <input type="hidden" name="action" value="dos-encoded">
       <button type="submit">Login</button>
     </div>
   </form> 
+<br><br><br>
+  <h2>Denial of service - 100ms delay per character, 2000ms delay for quotes</h2>
+<form method="post">
+    <div class="container">
+      <label for="name"><b>check if username is available</b></label>
+      <input type="text" placeholder="Enter Username" name="name" required>
+      <input type="hidden" name="action" value="dos-delay">
+  
+      <button type="submit">check</button>
+    </div>
+  </form> 
 
-  <?php
-  # instead of semicolom (') the server returns &#039; which is 6 times the original size
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $action = $_POST['action'];
+
+  switch ($action) {
+      case 'dos-delay':
+          dos_delay();
+          break;
+      case 'dos-encoded':
+          dos_encoded();
+          break;
+      default:
+          echo "something went wrong";
+          break;
+  }
+}
+
+    # instead of semicolom (') the server returns &#039; which is 6 times the original size
+    function dos_encoded() {
       $name = $_POST['uname'];
       echo "<p> user " . htmlspecialchars($name). " not found </p>";
     }
-  ?>
 
+    # foreach character 100ms delay to respond
+    # incase of quote add 2000ms
+    function dos_delay(){
+      $string = $_POST['name'];
+      foreach (str_split($string) as $char){
+        if($char === "'"){
+          usleep(2000000);
+        }else{
+          usleep(100000);
+        }
+      }
+      echo "schrodinger's username";
+    }
+  ?>
+  
 </body>
 </html>
 
