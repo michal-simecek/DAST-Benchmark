d2c1a15ff501bb15c8095510fc719ee0c27591db
Richardrr50 <richarddemunck@hotmail.com>
2024-04-12 09:11:18 +0200

    file upload / download fix and some improvements

--- modules/file-inclusion/site-file-inclusion/index.php
+++ modules/file-inclusion/site-file-inclusion/index.php
@@ -1,4 +1,3 @@
-
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploaded_file'])) {
     $targetDirectory = "uploads/";
@@ -14,10 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploaded_file'])) {
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
         echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
     } else {
+        // if everything is ok, try to upload file
         if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $targetFile)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES['uploaded_file']['name'])). " has been uploaded.";
+            echo "The file " . htmlspecialchars(basename($_FILES['uploaded_file']['name'])) . " has been uploaded.";
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
@@ -27,13 +26,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['uploaded_file'])) {
 
 <!DOCTYPE html>
 <html>
+<head>
+    <title>File Upload and Download</title>
+</head>
 <body>
 
+<h2>Upload File</h2>
 <form action="index.php" method="post" enctype="multipart/form-data">
-  Select file to upload:
-  <input type="file" name="uploaded_file" id="uploaded_file">
-  <input type="submit" value="Upload File" name="submit">
+    Select file to upload:
+    <input type="file" name="uploaded_file" id="uploaded_file">
+    <input type="submit" value="Upload File" name="submit">
 </form>
+
+<h2>Download Files</h2>
+<?php
+$files = scandir('uploads/');
+foreach ($files as $file) {
+    if ($file != "." && $file != "..") {
+        echo '<p><a href="uploads/' . urlencode($file) . '" download>' . htmlspecialchars($file) . '</a></p>';
+    }
+}
+?>
 
 </body>
 </html>
