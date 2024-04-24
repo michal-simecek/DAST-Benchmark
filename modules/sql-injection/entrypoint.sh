3ab59c2a3f17fe10d7bd2ae62c7c4fce02cde5f2
Richardrr50 <richarddemunck@hotmail.com>
2024-04-24 09:22:37 +0200

    fixed sqli

--- modules/sql-injection/entrypoint.sh
+++ modules/sql-injection/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-php-ext-install mysqli
-echo "extension=mongodb.so" >> /usr/local/etc/php/php.ini
-pecl install mongodb
-docker-php-ext-enable mongodb
+# echo "extension=mongodb.so" >> /usr/local/etc/php/php.ini
+# pecl install mongodb
+# docker-php-ext-enable mongodb
 exec docker-php-entrypoint php-fpm
