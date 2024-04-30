8308e7ec5689addf56a9cead54301b64833461c6
Richardrr50 <richarddemunck@hotmail.com>
2024-04-30 10:07:20 +0200

    added some files for idor

--- modules/error-detection/entrypoint.sh
+++ modules/error-detection/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Copy error pages
-cp -r /path/to/errors /var/www/html/errors
+cp -r /path/to/errors /var/www/html/pages
 
 # Ensure permissions are correct
-chown -R nginx:nginx /var/www/html/errors
+chown -R nginx:nginx /var/www/html/pages
 
 # Start Nginx in the foreground
 nginx -g 'daemon off;'
