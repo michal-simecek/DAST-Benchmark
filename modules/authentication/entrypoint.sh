8b7c778b366732fa35d4f16e297f49595142322b
Richardrr50 <richarddemunck@hotmail.com>
2024-05-03 10:09:49 +0200

    changes to main

--- modules/authentication/entrypoint.sh
+++ modules/authentication/entrypoint.sh
@@ -1,23 +1,23 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Install Apache2 utils if not already installed
-if ! command -v htpasswd &> /dev/null
-then
-    echo "test1"
-    apt-get update && apt-get install -y apache2-utils
-fi
+# # Install Apache2 utils if not already installed
+# if ! command -v htpasswd &> /dev/null
+# then
+#     echo "test1"
+#     apt-get update && apt-get install -y apache2-utils
+# fi
 
-# Ensure the Nginx config directory is writable or create the file as root
-if [ ! -f /etc/nginx/.htpasswd ]; then
-    touch /etc/nginx/.htpasswd
-fi
+# # Ensure the Nginx config directory is writable or create the file as root
+# if [ ! -f /etc/nginx/.htpasswd ]; then
+#     touch /etc/nginx/.htpasswd
+# fi
 
-# Create or update .htpasswd file
-htpasswd -cb /etc/nginx/.htpasswd username password
+# # Create or update .htpasswd file
+# htpasswd -cb /etc/nginx/.htpasswd username password
 
-# Change permissions to ensure Nginx can read the file
-chown www-data:www-data /etc/nginx/.htpasswd
-chmod 644 /etc/nginx/.htpasswd
+# # Change permissions to ensure Nginx can read the file
+# chown www-data:www-data /etc/nginx/.htpasswd
+# chmod 644 /etc/nginx/.htpasswd
 
-# Execute the default Nginx entrypoint and command (assumes Nginx is to be run in this container)
-exec nginx -g 'daemon off;'
+# # Execute the default Nginx entrypoint and command (assumes Nginx is to be run in this container)
+# exec nginx -g 'daemon off;'
---
+++ modules/authentication/site-authentication/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>authentication page index</title>
+</head>
+<body>
+<ul>
+    <li><a href=/pages/basicauth.php>basicauth.php</a></li>
+    <li><a href=/pages/jwt.php>jwt.php</a></li>
+</ul>
+</body>
+</head>
+<html>
