7eac9ab80b170f45ee5c096d3f8c0a92a4e6c9d1
Michal Simecek <m.simecek@accenture.com>
2024-08-14 07:29:28 -0400

    remove file inclusion module

--- modules/file-inclusion/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create uploads directory and set correct permissions
-mkdir -p /var/www/html/uploads
-chown www-data:www-data /var/www/html/uploads  #run this in the php file inclusion container in case of permission denied when uploading file
-chmod 755 /var/www/html/uploads                #run this in the php file inclusion container in case of permission denied when uploading file
-
-# Continue with container's main command
-exec docker-php-entrypoint php-fpm
