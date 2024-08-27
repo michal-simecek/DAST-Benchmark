7d686f60e8b3394963a637deed25cc58ab63bce8
Michal Simecek <m.simecek@accenture.com>
2024-08-27 06:16:37 -0400

    update basic auth page

--- modules/single-nginx/authentication/basic-auth.php
+++ modules/single-nginx/authentication/basic-auth.php
@@ -10,8 +10,18 @@ function validateCredentials($username, $password) {
 if (!isset($_SERVER['PHP_AUTH_USER']) || !validateCredentials($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'])) {
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
-    echo 'Unauthorized access'; // Display this message if authentication fails
+    echo 'Unauthorized access';
     exit;
 } else {
-    echo "<p>Welcome, {$_SERVER['PHP_AUTH_USER']}!</p>";
+    if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['message']) {
+        echo '<p>' . $_POST['message'] . '</p>';
+    } else {
+        echo '<form action="" method="post">';
+        echo "    <label for='message'>Enter your message:</label><br>";
+        echo "    <input type='text' id='message' name='message'><br>";
+        echo '    <input type="submit" value="Submit">';
+        echo '</form>';    
+    }
+
 }
+?>
